fa6bbf2859c787f3060e954807a902dbb2b0fa65
toofastdan117 <dddbrock4@gmail.com>
2024-12-29 05:20:57 -0600

    accept merge changes

--- js/footer0.js
+++ js/footer0.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // This is for the footer copyright
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
@@ -8,14 +7,3 @@ let today = `${currentDay}-${currentMonth}-${currentYear}`
 
 const copyright = document.getElementById('js-copyright')
 copyright.innerHTML = `&copy; ${currentYear} American Physician Scientists Association. All Rights Reserved.`
-=======
-// This is for the footer copyright
-let currentDate = new Date();
-let currentYear = currentDate.getFullYear();
-let currentMonth = currentDate.getMonth() + 1;
-let currentDay = currentDate.getDay();
-let today = `${currentDay}-${currentMonth}-${currentYear}`
-
-const copyright = document.getElementById('js-copyright')
-copyright.innerHTML = `&copy; ${currentYear} American Physician Scientists Association. All Rights Reserved.`
->>>>>>> b4de34a571cdeef601994180b8285a20ff1272f2
